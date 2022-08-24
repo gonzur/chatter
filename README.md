93699875621b74c11143bd1c105ea594110e6435
Juan Carlos Gonzalez-Zurita <juan.gonzur@gmail.com>
2022-08-23 23:20:11 -0700

    test linting

---
+++ README.md
@@ -0,0 +1,26 @@
+# Chatter
+
+> Yet another chat app made in react backed by go
+
+## Table of Contents
+
+- [General Info](#general-information)
+- [Technologies Used](#technologies-used)
+- [Setup](#setup)
+- [Project Status](#project-status)
+
+## General Information
+
+Made to show improvements in enterprise application development
+
+## Technologies Used
+
+- TODO
+
+## Setup
+
+- TODO
+
+## Project Status
+
+Project is: _in progress_
---
+++ chatter_client/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jest"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/react-in-jsx-scope": "off",
+  },
+};
