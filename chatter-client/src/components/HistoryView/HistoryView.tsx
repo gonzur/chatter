8a09a86d17709383e1d3324fba936632cefa50cb
Juan Carlos Gonzalez-Zurita <juan.gonzur@gmail.com>
2022-09-27 20:54:28 -0700

    redundant component removed

--- chatter-client/src/components/HistoryView/HistoryView.tsx
+++ chatter-client/src/components/HistoryView/HistoryView.tsx
@@ -5,20 +5,16 @@ interface HistoryViewProps {
   messages: Message[];
 }
 
-const MessageElement = ({ sender, sentOn, message }: Message) => (
-  <div className={styles.message}>
-    <div className={styles.status}>
-      <div>{sentOn}</div>
-      <div>{sender}:</div>
-    </div>
-    <div className={styles.text}>&quot;{message}&quot;</div>
-  </div>
-);
-
 const HistoryView = ({ messages }: HistoryViewProps) => (
   <div className={styles.view}>
     {messages.map(({ sender, message, sentOn }) => (
-      <MessageElement message={message} sender={sender} sentOn={sentOn} />
+      <div className={styles.message}>
+        <div className={styles.status}>
+          <div>{sentOn}</div>
+          <div>{sender}:</div>
+        </div>
+        <div className={styles.text}>&quot;{message}&quot;</div>
+      </div>
     ))}
   </div>
 );
