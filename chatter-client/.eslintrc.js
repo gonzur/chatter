12c1aef03243dd7976bdbb6672131d5bb075fcaf
Juan Carlos Gonzalez-Zurita <juan.gonzur@gmail.com>
2023-04-21 19:35:26 -0700

    fixup! vite added

--- chatter-client/.eslintrc.js
+++ chatter-client/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  ignorePatterns: ["node_modules/"],
   plugins: ["react", "@typescript-eslint", "jest"],
   rules: {
     "no-restricted-syntax": [0],
