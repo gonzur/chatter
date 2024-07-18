edd44d8ff4342a260b78149e96e26c5c91871494
Juan Gonzalez-Zurita <juan.gonzur@gmail.com>
2024-07-18 18:45:35 -0400

    grouped rooms

--- chatter-client/src/components/RoomBrowser/RoomBrowser.tsx
+++ chatter-client/src/components/RoomBrowser/RoomBrowser.tsx
@@ -17,8 +17,8 @@ const RoomBrowser = (props: RoomBrowserProps) => {
     { name: "room 1asdfdsf", memberCount: 9 },
     { name: "room 1", memberCount: 9 },
     { name: "room 1asdfsafdas", memberCount: 9 },
-    { name: "room 1", memberCount: 9 },
-    { name: "room 1", memberCount: 9 },
+    { name: "room 2", memberCount: 9 },
+    { name: "room 3", memberCount: 9 },
     { name: "1234567890123456", memberCount: 9 },
     { name: "wwwwwwwwwwwwwwww", memberCount: 9 },
   ]);
@@ -54,25 +54,27 @@ const RoomBrowser = (props: RoomBrowserProps) => {
           <AiOutlineSearch size="1.5rem" />
         </button>
       </div>
-      <div className={styles.roomList}>
-        {roomList
-          .filter((value) => value.name.includes(query))
-          .map((room) => (
-            <div id={room.name} className={styles.card}>
-              <span className={styles.active}>Active</span>
-              <h2 className={styles.roomName}>{room.name}</h2>
-              <div className={styles.bottomGroup}>
-                <p className={styles.members}>{room.memberCount} Users</p>
-                <button
-                  className={styles.joinButton}
-                  type="button"
-                  onClick={() => transition(room.name)}
-                >
-                  Join
-                </button>
+      <div className={styles.roomListBorder}>
+        <div className={styles.roomList}>
+          {roomList
+            .filter((value) => value.name.includes(query))
+            .map((room) => (
+              <div id={room.name} className={styles.card}>
+                <span className={styles.active}>Active</span>
+                <h2 className={styles.roomName}>{room.name}</h2>
+                <div className={styles.bottomGroup}>
+                  <p className={styles.members}>{room.memberCount} Users</p>
+                  <button
+                    className={styles.joinButton}
+                    type="button"
+                    onClick={() => transition(room.name)}
+                  >
+                    Join
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
     </div>
   );
